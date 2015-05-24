b985120d84d4e1f9ba4992df8f896a0b95c92260
jemirandas <jemirandas@unalmed.edu.co>
2015-05-24 08:47:49 -0500

    esto apesta

---
+++ classes/agenda.php
@@ -0,0 +1,60 @@
+<?php
+ 
+class agenda extends object_standard
+{
+	//attributes
+	protected $id;
+	protected $fecha;
+	protected $dia;
+	protected $hora;	
+	protected $disponibilidad;	
+	protected $empleado;
+	
+	//components
+	var $components = array();
+	
+	//auxiliars for primary key and for files
+	var $auxiliars = array();
+	
+	//data about the attributes
+	public function metadata()
+	{
+		return array("id" => array(), "fecha" => array(), "dia" => array(), "hora" => array(),  "disponibilidad" => array(),  "empleado" => array());
+	}
+		
+	public function primary_key()
+	{
+		return array("id");
+	}	
+	
+	public function relational_keys($class, $rel_name) 
+	{	
+		switch($class) 
+		{
+		case "empleado":
+		
+			switch($rel_name)
+			{
+				case "a_e":
+					return array("empleado");
+				break;
+			}
+			
+		break;
+
+		case "semana_de_agenda":
+		
+			switch($rel_name)
+			{
+				case "a_s":
+					return array("semana_de_agenda");
+				break;
+			}
+			
+		break;
+		
+		default:
+		break; 
+		} 
+	}
+}
